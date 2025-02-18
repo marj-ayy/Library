e5b498a3b6eb55e8b733c1f33a699a323df04565
Marouane <184505120+marj-ayy@users.noreply.github.com>
2025-02-18 16:45:12 +0100

    Add delete book functionality

--- script.js
+++ script.js
@@ -1,52 +1,27 @@
 const myLibrary = [];
 
-function Book(name, author, pages, read){
+function Book(name, author, pages, read) {
     this.name = name;
     this.author = author;
     this.pages = pages;
     this.read = read; // if the book is already read or not
 }
 
-function addBookToLibrary(name, author, pages, read){
+function addBookToLibrary(name, author, pages, read) {
     book = new Book(name, author, pages, read);
     myLibrary.push(book);
 }
-
-// addBookToLibrary("Harry Poter", "J. K. Rowlings", 400, true);
-// addBookToLibrary("The Hobbit", "J. R. R. Tolkien", 192, false);
-// addBookToLibrary("A Short Story Of Humanity", "Y. N. Harari", 450, true);
-
-// for(let i = 0; i < myLibrary.length; ++i){
-//     const table = document.querySelector("table");
-//     const row = document.createElement("tr");
-//     const tdName = document.createElement("td");
-//     const tdAuthor = document.createElement("td");
-//     const tdPages = document.createElement("td");
-//     const tdRead = document.createElement("td");
-//     row.appendChild(tdName);
-//     row.appendChild(tdAuthor);
-//     row.appendChild(tdPages);
-//     row.appendChild(tdRead);
-//     table.appendChild(row);
-//     tdName.textContent = myLibrary[i].name;
-//     tdAuthor.textContent = myLibrary[i].author;
-//     tdPages.textContent = myLibrary[i].pages;
-//     if(myLibrary[i].read === true)
-//         tdRead.textContent = "Yes";
-//     else
-//         tdRead.textContent = "No";
-// }
 
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector(".showDialogButton");
 const closeButton = document.querySelector(".closeDialogButton");
 
 showButton.addEventListener("click", () => {
-  dialog.showModal();
+    dialog.showModal();
 });
 
 closeButton.addEventListener("click", () => {
-  dialog.close();
+    dialog.close();
 });
 
 
@@ -58,30 +33,41 @@ form.addEventListener("submit", event => {
     const authorInput = document.getElementById("author");
     const pagesInput = document.getElementById("pages");
     const readNoInput = document.getElementById("readNo");
-    console.log(nameInput.value);
-    if(readNoInput.checked)
-        addBookToLibrary(nameInput.value, authorInput.value, pagesInput.value, false);
+    if (readNoInput.checked)
+        addBookToLibrary(nameInput.value, authorInput.value, parseInt(pagesInput.value), false);
     else
-        addBookToLibrary(nameInput.value, authorInput.value, pagesInput.value, true);
-    console.log(myLibrary);
+        addBookToLibrary(nameInput.value, authorInput.value, parseInt(pagesInput.value), true);
     const table = document.querySelector("table");
     const row = document.createElement("tr");
+    row.classList.add("book-" + (myLibrary.length - 1).toString());
     const tdName = document.createElement("td");
     const tdAuthor = document.createElement("td");
     const tdPages = document.createElement("td");
     const tdRead = document.createElement("td");
+    const tdDelete = document.createElement("td");
+    const deleteBookButton = document.createElement("button");
+    deleteBookButton.type = "button";
+    deleteBookButton.classList.add("deleteBookButton");
+    deleteBookButton.textContent = "Delete";
+    tdDelete.appendChild(deleteBookButton);
     row.appendChild(tdName);
     row.appendChild(tdAuthor);
     row.appendChild(tdPages);
     row.appendChild(tdRead);
+    row.appendChild(tdDelete);
     table.appendChild(row);
     tdName.textContent = myLibrary[myLibrary.length - 1].name;
     tdAuthor.textContent = myLibrary[myLibrary.length - 1].author;
     tdPages.textContent = myLibrary[myLibrary.length - 1].pages;
-    if(myLibrary[myLibrary.length - 1].read === true)
+    if (myLibrary[myLibrary.length - 1].read === true)
         tdRead.textContent = "Yes";
     else
         tdRead.textContent = "No";
     form.reset();
-  });
+    deleteBookButton.addEventListener("click", event => {
+        const bookNumber = parseInt(event.target.parentElement.parentElement.classList[0].slice(-1));
+        event.target.parentElement.parentElement.remove();
+        myLibrary.splice(bookNumber, 1); 
+    });
+});
 
