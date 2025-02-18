70319b38a76830d8f33673cf0c0ce10c5cd985f5
Marouane <184505120+marj-ayy@users.noreply.github.com>
2025-02-18 11:46:49 +0100

    Add books display functionality to page

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ function addBookToLibrary(name, author, pages, read){
     myLibrary.push(book);
 }
 
+addBookToLibrary("Harry Poter", "J. K. Rowlings", 400, true);
+addBookToLibrary("The Hobbit", "J. R. R. Tolkien", 192, false);
+addBookToLibrary("A Short Story Of Humanity", "Y. N. Harari", 450, true);
+
+for(let i = 0; i < myLibrary.length; ++i){
+    const table = document.querySelector("table");
+    const row = document.createElement("tr");
+    const tdName = document.createElement("td");
+    const tdAuthor = document.createElement("td");
+    const tdPages = document.createElement("td");
+    const tdRead = document.createElement("td");
+    row.appendChild(tdName);
+    row.appendChild(tdAuthor);
+    row.appendChild(tdPages);
+    row.appendChild(tdRead);
+    table.appendChild(row);
+    tdName.textContent = myLibrary[i].name;
+    tdAuthor.textContent = myLibrary[i].author;
+    tdPages.textContent = myLibrary[i].pages;
+    if(myLibrary[i].read === true)
+        tdRead.textContent = "Yes";
+    else
+        tdRead.textContent = "No";
+}
